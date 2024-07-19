21bc62f05d004c0bb12e0de23bd8de6c0565f55c
rach syahputra <rach.syahputra@gmail.com>
2024-07-19 21:21:52 +0800

    chore: add CopyWebpackPlugin configuration in webpack.common.js

--- webpack.common.js
+++ webpack.common.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 const htmlWebpackPluginConfig = {
@@ -79,6 +80,15 @@ module.exports = {
       filename: 'index.html',
       template: path.resolve(__dirname, 'src/views/dashboard.html'),
       ...htmlWebpackPluginConfig,
+    }),
+
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, 'src/public/'),
+          to: path.resolve(__dirname, 'dist/'),
+        },
+      ],
     }),
 
     new CleanWebpackPlugin(),
