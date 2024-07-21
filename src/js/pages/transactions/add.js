0594cedf67ebf67aacca2e88a0c7bca7cbcf78d7
rach syahputra <rach.syahputra@gmail.com>
2024-07-21 22:55:40 +0800

    refactor: remove unnecessary functions in add.js

--- src/js/pages/transactions/add.js
+++ src/js/pages/transactions/add.js
@@ -27,11 +27,6 @@ const Add = {
   },
 
   _initialListener() {
-    const evidenceInput = document.querySelector('#validationCustomEvidence')
-    evidenceInput.addEventListener('change', () => {
-      this._updatePhotoPreview()
-    })
-
     const addFormRecord = document.querySelector('#addRecordForm')
     addFormRecord.addEventListener(
       'submit',
@@ -73,22 +68,6 @@ const Add = {
       description: descriptionInput.value,
       type: typeInput.value,
     }
-  },
-
-  _updatePhotoPreview() {
-    const evidenceImgChange = document.querySelector('#validationCustomEvidenceImgChange')
-    const evidenceImgInput = document.querySelector('#validationCustomEvidence')
-
-    const photo = evidenceImgInput.files[0]
-    if (!photo) return
-
-    const reader = new FileReader()
-    reader.onload = (event) => {
-      evidenceImgChange.parentElement.classList.remove('d-none')
-      evidenceImgChange.style.backgroundImage = `url('${event.target.result}')`
-    }
-
-    reader.readAsDataURL(photo)
   },
 
   _validateFormData(formData) {
