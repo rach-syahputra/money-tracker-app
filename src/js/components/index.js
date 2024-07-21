9028b3bd9a49a444a9fe76484e9a0d66bb6372ee
rach syahputra <rach.syahputra@gmail.com>
2024-07-21 20:42:30 +0800

    chore: import all components to component's entry point

--- src/js/components/index.js
+++ src/js/components/index.js
@@ -1,1 +1,8 @@
 import './CardDashboard'
+import './ButtonLink'
+import './ModalTransaction'
+import './NavApp'
+import './NavLinks'
+import './NavLink'
+import './NavLinkAuth'
+import './FooterApp'
