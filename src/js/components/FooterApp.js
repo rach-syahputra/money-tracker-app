147fccd06836c9e034bc0dcc28683eab79aea825
rach syahputra <rach.syahputra@gmail.com>
2024-07-22 13:12:58 +0800

    feat: implement msg to footer-app component for localization

--- src/js/components/FooterApp.js
+++ src/js/components/FooterApp.js
@@ -1,9 +1,17 @@
 import { html } from 'lit'
 import LitWithoutShadowDom from './base/LitWithoutShadowDom'
+import { msg, updateWhenLocaleChanges } from '@lit/localize'
 
 class FooterApp extends LitWithoutShadowDom {
+  constructor() {
+    super()
+    updateWhenLocaleChanges(this)
+  }
+
   render() {
-    return html` <p class="text-center text-white mb-0">Made with ❤ by Dicoding Indonesia</p> `
+    return html`
+      <p class="text-center text-white mb-0">${msg(`Dibuat dengan ❤ oleh Dicoding Indonesia`)}</p>
+    `
   }
 }
 
