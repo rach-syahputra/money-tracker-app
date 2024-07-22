e6169a652761a649c6a80903e7e4f0118e4da9c8
rach syahputra <rach.syahputra@gmail.com>
2024-07-22 13:20:29 +0800

    feat: implement localization for nav-links component

--- src/js/components/NavLinks.js
+++ src/js/components/NavLinks.js
@@ -1,14 +1,20 @@
 import { html } from 'lit'
 import LitWithoutShadowDom from './base/LitWithoutShadowDom'
+import { msg, updateWhenLocaleChanges } from '@lit/localize'
 
 class NavLinks extends LitWithoutShadowDom {
+  constructor() {
+    super()
+    updateWhenLocaleChanges(this)
+  }
+
   render() {
     return html`
       <ul class="navbar-nav d-flex align-items-center gap-3">
-        <nav-link content="Dashboard" to="/"></nav-link>
-        <nav-link content="Add Record" to="/transactions/add.html"></nav-link>
+        <nav-link content="${msg(`Dasbor`)}" to="/"></nav-link>
+        <nav-link content="${msg(`Tambah Data`)}" to="/transactions/add.html"></nav-link>
         <nav-link-auth class="d-none" id="userLoggedMenu"></nav-link-auth>
-        <nav-link content="Log In" to="#" id="loginMenu"></nav-link>
+        <nav-link content="${msg(`Masuk`)}" to="#" id="loginMenu"></nav-link>
       </ul>
     `
   }
