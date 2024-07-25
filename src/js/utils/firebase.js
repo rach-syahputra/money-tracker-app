d37e4437509f9498f5deb476bd976ca8378dff44
rach syahputra <rach.syahputra@gmail.com>
2024-07-25 20:14:44 +0800

    initialize firebase SDK and configuration

---
+++ src/js/utils/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyAyD0h3OqUfIp46tdR5ZmX0DqsZwNPtgxQ',
+  authDomain: 'money-tracker-app-8bec9.firebaseapp.com',
+  projectId: 'money-tracker-app-8bec9',
+  storageBucket: 'money-tracker-app-8bec9.appspot.com',
+  messagingSenderId: '1043988389294',
+  appId: '1:1043988389294:web:e8865b0d90a74396927c3f',
+}
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
